ab27a779c1d02321628aa39055607fa4a32e665b
Istiniel <eclipsio@mail.ru>
2023-04-04 22:01:55 +0300

    feat: add react-portal

--- src/components/ModalAnime/index.tsx
+++ src/components/ModalAnime/index.tsx
@@ -3,6 +3,7 @@ import st from './ModalAnime.module.scss';
 import useRemoveDisplayShift from './../../hooks/useRemoveDisplayShift';
 import useFetchAnimeById from '../../hooks/useFetchAnimeById';
 import Spinner from './../Spinner/index';
+import Portal from '../Portal';
 
 type ModalAnimeProps = {
   closeModal: () => void;
@@ -15,39 +16,41 @@ const ModalAnime: React.FC<ModalAnimeProps> = ({ closeModal, animeId }) => {
   useRemoveDisplayShift();
 
   return (
-    <div className={st.container} onClick={closeModal}>
-      {loading && <Spinner />}
-      {!loading && (
-        <div className={st.modal} onClick={(e: React.MouseEvent) => e.stopPropagation()}>
-          <button className={st.closeButton} onClick={closeModal}>
-            +
-          </button>
-          <div className={st.thumbContainer}>
-            <img src={anime?.images.jpg.large_image_url || ''} alt="thumb" className={st.thumb} />
+    <Portal>
+      <div className={st.container} onClick={closeModal}>
+        {loading && <Spinner />}
+        {!loading && (
+          <div className={st.modal} onClick={(e: React.MouseEvent) => e.stopPropagation()}>
+            <button className={st.closeButton} onClick={closeModal}>
+              +
+            </button>
+            <div className={st.thumbContainer}>
+              <img src={anime?.images.jpg.large_image_url || ''} alt="thumb" className={st.thumb} />
+            </div>
+            <div className={st.infoBlock}>
+              <h2 className={st.fullTitle}>{anime?.title}</h2>
+              <h3 className={st.id}>{anime?.mal_id}</h3>
+              <h3 className={st.score}>
+                Score: <span>{anime?.score || '-'}</span>
+              </h3>
+              <h3 className={st.rank}>
+                Rank: <span>{anime?.rank || '-'}</span>
+              </h3>
+              <h3 className={st.type}>
+                Type: <span>{anime?.type || '-'}</span>
+              </h3>
+              <h3 className={st.season}>
+                Season: <span>{anime?.season || '-'}</span>
+              </h3>
+              <h3 className={st.status}>
+                Status: <span>{anime?.status || '-'}</span>
+              </h3>
+              <p className={st.synopsis}>{anime?.synopsis}</p>
+            </div>
           </div>
-          <div className={st.infoBlock}>
-            <h2 className={st.fullTitle}>{anime?.title}</h2>
-            <h3 className={st.id}>{anime?.mal_id}</h3>
-            <h3 className={st.score}>
-              Score: <span>{anime?.score || '-'}</span>
-            </h3>
-            <h3 className={st.rank}>
-              Rank: <span>{anime?.rank || '-'}</span>
-            </h3>
-            <h3 className={st.type}>
-              Type: <span>{anime?.type || '-'}</span>
-            </h3>
-            <h3 className={st.season}>
-              Season: <span>{anime?.season || '-'}</span>
-            </h3>
-            <h3 className={st.status}>
-              Status: <span>{anime?.status || '-'}</span>
-            </h3>
-            <p className={st.synopsis}>{anime?.synopsis}</p>
-          </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </Portal>
   );
 };
 
